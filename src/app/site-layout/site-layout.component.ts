353ccf520d61a7d2dd6261327bbbed8f7ebd736e
seineo <1013248644@qq.com>
2023-03-07 20:51:58 +0800

    truncate long file name like mac style

--- src/app/site-layout/site-layout.component.ts
+++ src/app/site-layout/site-layout.component.ts
@@ -10,9 +10,32 @@ export class SiteLayoutComponent {
   testData:string[] = [];
 
   constructor(private router: Router) {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 5; i++) {
       this.testData.push("file" + i.toString() + ".txt");
     }
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hello-world!-I-am-liyuewei.txt");
+    this.testData.push("hi.txt");
+
+  }
+
+  truncateMiddle(word: string) {
+    const tooLongChars = 20; // arbitrary
+
+    if (word.length < tooLongChars) {
+      return word;
+    }
+
+    const ellipsis = '...';
+    const charsOnEitherSide = Math.floor(tooLongChars / 2) - ellipsis.length;
+
+    return word.slice(0, charsOnEitherSide) + ellipsis + word.slice(-charsOnEitherSide);
   }
 
   onDBClick() {
