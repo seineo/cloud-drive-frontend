f1164597500146d6c639f7e8c9047d5d3b5bf17b
seineo <1013248644@qq.com>
2023-07-22 23:56:48 +0800

    add model file
    used to define interfaces

---
+++ src/app/file.model.ts
@@ -0,0 +1,39 @@
+export enum UploadingStatus {
+  WAITING,
+  UPLOADING,
+  COMPLETED
+}
+
+export interface UploadingFile {
+  name: string,
+  status: UploadingStatus,
+  progress: number
+}
+
+export interface DirRequest {
+  hash: string,
+  name: string,
+  dirHash: string
+}
+
+export interface FileRequest {
+  fileHash: string,
+  fileName: string,
+  fileType: string,
+  dirHash: string,
+  fileSize: number
+}
+
+export interface MyFile {
+    hash: string,
+    name: string
+    type: string // dir, pdf, img, video...
+    size: number
+    createdAt: string
+}
+
+export interface MyDir {
+    hash: string,
+    name: string,
+    createdAt: string
+}
